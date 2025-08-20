60e05ad7261fca9c0ff80e9133720e151ad240ac
NohraJSeif <nseif89@hotmail.com>
2025-08-20 14:06:09 -0400

    adding investment results in a table

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
-  styleUrl: './investment-results.component.css'
+  styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-
+  // results = input<{  year: number;
+  // interest: number;
+  // valueEndOfYear: number;
+  // annualInvestment: number;
+  // totalInterest: number;
+  // totalAmountInvested: number;}>()
+  @Input() results?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
 }
